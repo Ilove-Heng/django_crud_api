997c606cc6e8e47f01a293afe7da1d65de3477a0
nangseakheng <seakhengclever@gmail.com>
2023-08-12 15:25:59 +0700

    image preview

--- django_api_task/settings.py
+++ django_api_task/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,9 +52,19 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'django_api_task.urls'
+CORS_ORIGIN_ALLOW_ALL = True
+
+
+
+
+
+# CORS_ORIGIN_WHITELIST = (
+#   'http://localhost:8000',
+# )
 
 TEMPLATES = [
     {
